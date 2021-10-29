353ab4aa221712be743f117e011c232d1b5aac48
k.struzhkin <koztyastruga@gmail.com>
2021-10-29 10:32:42 +0300

    Fix servlet init

--- src/test/java/com/rbkmoney/dominant/cache/DominantCacheApplicationTest.java
+++ src/test/java/com/rbkmoney/dominant/cache/DominantCacheApplicationTest.java
@@ -1,18 +1,15 @@
 package com.rbkmoney.dominant.cache;
 
-import org.apache.thrift.TException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.net.URISyntaxException;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = DominantCacheApplication.class)
 public class DominantCacheApplicationTest {
 
     @Test
-    public void contextLoads() throws URISyntaxException, TException {
+    public void contextLoads() {
     }
 }
